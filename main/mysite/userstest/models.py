5f2210e020784c4a6e5c1a817fcd605536512276
prestonsaulsbury <prestonjsaulsbury@gmail.com>
2024-10-04 22:52:28 -0700

    Create migration for user's model

--- main/mysite/userstest/models.py
+++ main/mysite/userstest/models.py
@@ -16,3 +16,8 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
